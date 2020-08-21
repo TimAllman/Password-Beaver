4d72b14b4a1e5e080c23192f902e5fde66f872df
Tim Allman <dr.tim.allman@gmail.com>
2020-08-21 14:18:25 -0400

    Translation continued and included in resource.

---
+++ locale/pwbeaver_fr.ts
@@ -0,0 +1,325 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../aboutdialog.ui" line="17"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Thanks to &lt;a href=&quot;https://www.flaticon.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;Flaticon&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;for the beaver icon&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="vanished">html&gt; &lt;head /&gt; &lt;body&gt; &lt;p align = &quot;center&quot;&gt; Merci à &lt;a href=&quot;https://www.flaticon.com&quot;&gt; &lt;span style = &quot;text-decoration: underline; color: # 0057ae ; &quot;&gt; Flaticon &lt;/span&gt; &lt;/a&gt; &lt;br/&gt; pour l&apos;icône du castor &lt;/p&gt; &lt;/body&gt; &lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="59"/>
+        <location filename="../aboutdialog.ui" line="223"/>
+        <location filename="../aboutdialog.ui" line="226"/>
+        <source>Open Password Beaver&apos;s Web site.</source>
+        <translation>Ouvrir le site Web de Password Beaver.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="158"/>
+        <source>A secure password generator.</source>
+        <oldsource>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A secure password generator.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</oldsource>
+        <translation>Un générateur de mot de passe sécurisé.</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="176"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Released under the&lt;br/&gt;GNU Public License V3.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <oldsource>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Released under the&lt;br/&gt;GNU General Public License V3.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</oldsource>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Publié sous la&lt;br/&gt;GNU General Public License V3.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.ui" line="204"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Thanks to &lt;a href=&quot;https://www.flaticon.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;Flaticon &lt;/span&gt;&lt;/a&gt;for the beaver icon&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt; &lt;p&gt; Merci à &lt;a href=&quot;https://www.flaticon.com&quot;&gt; &lt;span style = &quot;text-decoration: underline; color: # 0057ae;&quot;&gt; Flaticon &lt;/span&gt; &lt;/a&gt; pour l&apos;icône du castor &lt;/p&gt; &lt;/body&gt; &lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../AboutDialog.cpp" line="48"/>
+        <source>Unable to open URL: </source>
+        <translation>Impossible d&apos;ouvrir l&apos;URL: </translation>
+    </message>
+</context>
+<context>
+    <name>HelpDialog</name>
+    <message>
+        <location filename="../helpdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../helpdialog.ui" line="40"/>
+        <source>Password Beaver Help</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../helpdialog.ui" line="97"/>
+        <source>Close</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="31"/>
+        <source>Options</source>
+        <translation>Options</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="39"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select upper case letters - [&lt;span style=&quot; font-weight:600;&quot;&gt;A-Z&lt;/span&gt;]&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sélectionner les lettres majuscules - [&lt;span style=&quot; font-weight:600;&quot;&gt;A-Z&lt;/span&gt;]&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="42"/>
+        <source>Upper case letters</source>
+        <translation>Lettres majuscules</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="52"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select lower case letters - [&lt;span style=&quot; font-weight:600;&quot;&gt;a-z&lt;/span&gt;]&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sélectionner les lettres minuscules - [&lt;span style=&quot; font-weight:600;&quot;&gt;a-z&lt;/span&gt;]&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="55"/>
+        <source>Lower case letters</source>
+        <translation>Letters minuscules</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="65"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select digits - [&lt;span style=&quot; font-weight:600;&quot;&gt;0-9&lt;/span&gt;]&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sélectionner des chiffres - [&lt;span style=&quot; font-weight:600;&quot;&gt;0-9&lt;/span&gt;]&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="68"/>
+        <source>Digits</source>
+        <translation>Chiffres</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="78"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select punctuation characters - [&lt;span style=&quot; font-weight:600;&quot;&gt;!&amp;quot;#%&amp;amp;&apos;()*,-./:;?@[\]_{}&lt;/span&gt;]&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sélectionner les caractères de ponctuation - [&lt;span style=&quot; font-weight:600;&quot;&gt;!&amp;quot;#%&amp;amp;&apos;()*,-./:;?@[\]_{}&lt;/span&gt;]&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="81"/>
+        <source>Punctuation</source>
+        <translation>Ponctuation</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="91"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select symbol characters - [&lt;span style=&quot; font-weight:600;&quot;&gt;$+&amp;lt;=&amp;gt;^`|~&lt;/span&gt;]&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sélectionner les caractères de symbole - [&lt;span style=&quot; font-weight:600;&quot;&gt;$+&amp;lt;=&amp;gt;^`|~&lt;/span&gt;]&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="94"/>
+        <source>Symbols</source>
+        <translation>Symboles</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="104"/>
+        <source>Include 8859-1 (Latin 1) characters.</source>
+        <translation>Incluer les caractères 8859-1 (Latin 1).</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="107"/>
+        <source>Extended ASCII</source>
+        <translation>ASCII étendu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="122"/>
+        <source>Exclude</source>
+        <translation>Excluer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="158"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Exclude the characters entered here.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Excluer les caractères saisis ici.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="172"/>
+        <source>Size of character pool:</source>
+        <translation>Taille du jeu de caractères:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="218"/>
+        <source>Copy to clipboard</source>
+        <translation>Copier dans le presse-papier</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="359"/>
+        <source>Entropy:</source>
+        <translation>Entropie:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="459"/>
+        <source>Manual</source>
+        <translation>Manuel</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="129"/>
+        <source>Length</source>
+        <translation>Longueur</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Set the length of the password - 8-99&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Définissez la longueur du mot de passe - 8-99.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="142"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Set the length of the password - 8-99.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Définissez la longueur du mot de passe - 8-99.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Size of character pool: </source>
+        <translation type="vanished">Taille du jeu de caractères</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;The number of characters available for password generation. For information only.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Le nombre de caractères disponibles pour la génération de mot de passe. Pour information uniquement.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="215"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Automatically copy password to clipboard.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Copie automatiquement le mot de passe dans le presse-papiers.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Copy to Clipboard</source>
+        <translation type="vanished">Copier dans le presse-papier</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="230"/>
+        <source>Configurations</source>
+        <translation>Configurations</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="244"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Enter the name of the configuration to select, create or delete.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saisissez le nom de la configuration à sélectionner, créer ou supprimer.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="250"/>
+        <source>Default</source>
+        <translation>Défaut</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="272"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Options are unsaved if active.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Les options ne sont pas enregistrées si actif.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="285"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Save the current options to the database.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Enregistrez les options actuelles dans la base de données.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="288"/>
+        <source>Save</source>
+        <translation>Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="295"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Delete the current options from the database.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Supprimez les options actuelles de la base de données.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="298"/>
+        <source>Delete</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="330"/>
+        <source>Password</source>
+        <translation>Mot de passe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="343"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;The generated password.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Le mot de passe généré.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Entropy</source>
+        <translation type="vanished">Entropie</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="366"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt; The entropy of the password. For information only.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;L&apos;entropie du mot de passe. Pour information uniquement.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="392"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Create a new password.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Créez un nouveau mot de passe.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="395"/>
+        <source>Generate</source>
+        <translation>Générer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="408"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Exit &lt;span style=&quot; font-weight:600;&quot;&gt;Password Beaver&lt;/span&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Quitter &lt;span style=&quot; font-weight:600;&quot;&gt;Password Beaver&lt;/span&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="414"/>
+        <location filename="../mainwindow.ui" line="454"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="436"/>
+        <source>File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="442"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation type="vanished">Quitter</translation>
+    </message>
+    <message>
+        <source>Ctrl+Q</source>
+        <translation type="vanished">Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="477"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="468"/>
+        <source>About Qt</source>
+        <translation>À propos de Qt</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="279"/>
+        <location filename="../MainWindow.cpp" line="295"/>
+        <source>Password Beaver Error</source>
+        <translation>Erreur de Password Beaver</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="281"/>
+        <source>Some required characters are not available because they have been manually excluded. Remove at least one of these characters from the &quot;Exclude&quot; editor or use the check box to turn off this feature.</source>
+        <oldsource>Some required characters are not available because they have been manually excluded. Remove at least one of these characters from the &quot;Exclude these characters&quot; editor or use the check box to turn off this feature.</oldsource>
+        <translation>Des caractères obligatoires ne sont pas disponibles car ils ont été exclus manuellement. Supprimez au moins un de ces caractères de l&apos;éditeur &quot;Exclurer&quot; ou utilisez la case à cocher pour désactiver cette fonctionnalité.</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="297"/>
+        <source>The character pool from which the password is derived is too small. Add more character classes or remove some from the exclusion list.</source>
+        <translation>Le jeu de caractères à partir duquel le mot de passe est dérivé est trop petit. Ajoutez d&apos;autres classes de caractères ou supprimez-en certaines de la liste d&apos;exclusion.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../PasswordGenerator.cpp" line="131"/>
+        <source>The character pool has too few (%1) characters.</source>
+        <translation>Le jeu de caractères contient trop peu de (% 1) caractères.</translation>
+    </message>
+</context>
+</TS>
